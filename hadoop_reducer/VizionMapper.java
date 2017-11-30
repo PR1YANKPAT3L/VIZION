674e600f81644e309e399e52fb06e9be99f4c8b2
Priyank Patel <priyankpatel4@hotmail.com>
2017-11-30 13:01:38 -0500

    Add Logger

--- hadoop_reducer/VizionMapper.java
+++ hadoop_reducer/VizionMapper.java
@@ -1,4 +1,20 @@
 package hadoop_reducer;
 
-public class VizionMapper extends Mapper {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class VizionMapper extends Mapper<Text, Video, Text, Text> {
+
+    // Declare LOG for logging class runtime
+    private static final Log LOG = LogFactory.getLog(VizionMapper.class);
+
+    @Override
+    protected void map(Text key, Video value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
