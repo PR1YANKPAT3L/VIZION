312964789f6a75c8dea9bf6d0ab3d4ff81cca081
PR1YANKPAT3L <silentcoder@live.ca>
2017-12-04 11:50:50 -0500

    add vizion mapper

--- hadoop_reducer/VizionMapper.java
+++ hadoop_reducer/VizionMapper.java
@@ -22,12 +22,6 @@ public class VizionMapper extends Mapper<Text, Video, Text, Text> {
 
         String folder_ref = key.toString().split("\\.")[0].replaceAll("[\\D]", "");
 
-        File directory = new File("/home/shankz/CROPPER/pic_" + folder_ref);
-        directory.mkdir();
-
-        File directory2 = new File("/home/shankz/CROPPER/cropped_" + folder_ref);
-        directory2.mkdir();
-
         String output = null;
 
         int total_seconds = 0;
@@ -35,7 +29,6 @@ public class VizionMapper extends Mapper<Text, Video, Text, Text> {
         int id = value.getUid();
 
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(value.getVideoBytesArray());
-        LOG.info("LOG: VizionMapper_byteArray: " + byteArrayInputStream.available());
 
         int flag = 0;
         int video_num = 0;
@@ -45,20 +38,11 @@ public class VizionMapper extends Mapper<Text, Video, Text, Text> {
         Path local_file_path = new Path("/tmp/");
         hdfs.copyToLocalFile(hdfs_file_path, local_file_path);
 
-        hdfs_file_path = new Path("/user/shankz/input/timer.txt");
-        local_file_path = new Path("/tmp/");
-        hdfs.copyToLocalFile(hdfs_file_path, local_file_path);
-
         String file_name = key.toString();
-
-        // Process inputData = Runtime.getRuntime().exec("/usr/local/hadoop/bin/hdfs dfs -get /home/shankz/hadoop/test/input /tmp");
-        // inputData.waitFor();
 
         LocalFileSystem fileSystem = FileSystem.getLocal(context.getConfiguration());
         Path file_path = new Path("/tmp", file_name);
         Path res_file = new Path("/tmp", "res_" + file_name);
-
-        System.out.println("File to Process: " + file_path.toString());
 
         FSDataOutputStream dataOutputStream = fileSystem.create(file_path, true);
         output = file_path.toString().split("/")[2];
@@ -69,10 +53,8 @@ public class VizionMapper extends Mapper<Text, Video, Text, Text> {
 
         try {
             ProcessBuilder ffmpeg = new ProcessBuilder("avconv -i " + file_path.toString() + " -r 1 -f image2 /user/shankz/CROPPER/pic_" + folder_ref + "/image-%3d.jpg");
-            // ProcessBuilder ffmpeg = new ProcessBuilder("/usr/bin/avconv -i /user/shankz/hadoop/test/input/video_1.mkv -r 1 -f image2 /user/shankz/CROPPER/pic_" + folder_ref + "/image-%3d.jpg");
              ffmpeg.redirectErrorStream(true);
              Process ffmpegProcess = ffmpeg.start();
-
 
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(ffmpegProcess.getInputStream()));
             String linez = null;
@@ -96,7 +78,6 @@ public class VizionMapper extends Mapper<Text, Video, Text, Text> {
             ex.printStackTrace();
             stringBuilder.append("FAILED: " + ex.getMessage());
         }
-
 
         stringBuilder.append(folder_ref);
 
